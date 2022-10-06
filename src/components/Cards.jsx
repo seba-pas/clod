3c251afdb8e63f5d296ec1edb50bd9dd8ad779e3
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2022-10-05 21:08:07 -0300

    responsive

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -15,7 +15,7 @@ function Cards() {
         ropa.map((e) => {
           return (
             <Col className="col-3  " >
-              <Card style={{ marginTop: "20px", height: '40rem'}}>
+              <Card className='carta' style={{ marginTop: "20px", height: '28rem'}}>
                 <Card.Img
                   variant="top"
                   src={e.imagen} 
@@ -23,10 +23,10 @@ function Cards() {
                              />
                 <Card.Body>
                   <Card.Title>{e.nombre}</Card.Title>
-                  <span>Hecho en {e.tela}</span>
-                  <span>Colores Disponibles: {e.color.join(", ")}</span>
-                  <span>Talles Disponibles: {e.talles.join(", ")}</span>
-                  <span>Guia de taller: 1(M-L) - 2(L-XL) - 3(3XL)</span>
+                  <small>Hecho en: {e.tela}</small>
+                  <small>Colores Disponibles: {e.color.join(", ")}</small>
+                  <small>Talles Disponibles: {e.talles.join(", ")}</small>
+                  <small>Guia de taller: 1(M-L) - 2(L-XL) - 3(3XL)</small>
                 </Card.Body>
                 <Card.Footer>
                   <small className="text-muted" style={{fontFamily: 'clod'}}>CLÖD indumentaria</small>
